9797ea5071c36ad3f516d3289a576acf49a3e33d
KatchauBerg <kevinlima138@gmail.com>
2024-10-15 17:48:45 -0300

    subindo alteracao codigo inteiro

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -13,10 +13,14 @@ local on_attach = function(_, _)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
 end
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 require("lspconfig").lua_ls.setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = capabilities
 }
 
 require("lspconfig").intelephense.setup {
-  on_attach = on_attach
+  on_attach = on_attach,
+  capabilities = capabilities
 }
